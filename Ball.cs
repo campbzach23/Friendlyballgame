4fc0a4765c4862c86380f7b5fe85233f793c40a1
campbzach23 <77900413+campbzach23@users.noreply.github.com>
2021-01-23 13:23:08 -0800

    Ball.cs
    Physics on the ball

---
+++ Ball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    float _speed = 30f;
+    Rigidbody _rigidBody;
+    Vector3 _velocity;
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.velocity = Vector3.down * _speed;
+
+    }
+
+
+    void FixedUpdate()
+    {
+        _rigidBody.velocity = _rigidBody.velocity.normalized * _speed;
+        _velocity = _rigidBody.velocity;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rigidBody.velocity = Vector3.Reflect(_velocity, collision.contacts[0].normal);
+    }
+
+}
